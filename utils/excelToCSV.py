f7fb0139fede3be4fa6bd2e01eea57a415578f86
JacobiClark <jacobcs@zoho.com>
2020-04-26 13:16:57 -0700

    updated CSV

--- utils/excelToCSV.py
+++ utils/excelToCSV.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-read_file = pd.read_excel (r'C:\Users\Jacobi\programming\projects\WGUPS\assets\rawAssets\WGUPS Package File.xlsx')
-read_file.to_csv (r'C:\Users\Jacobi\programming\projects\WGUPS\assets\csv\packages.csv', index = None, header=True)
+read_file = pd.read_excel (r'C:\Users\Jacobi\programming\projects\WGUPS\assets\rawAssets\WGUPS Distance Table.xlsx')
+read_file.to_csv (r'C:\Users\Jacobi\programming\projects\WGUPS\assets\csv\distances.csv', index = None, header=True)
