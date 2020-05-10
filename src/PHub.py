d93a83f2d65420dae51b5163f5133a869a9adeb0
JacobiClark <jacobcs@zoho.com>
2020-05-09 18:02:24 -0700

    test

--- src/PHub.py
+++ src/PHub.py
@@ -1,44 +1,13 @@
 import csv
 import Truck
-#from Graph import Graph, Location, Package
+import Model
 import datetime
 from HashTable import HashTable
 import collections
 # Hub class
-class Location():
-    def __init__(self, location_ID, name, address, zip_code):
-        self.adjacent_paths = {}
-        self.location_ID = location_ID
-        self.name = name
-        self.address = address
-        self.zip_code = zip_code
-
-    def add_adjacent_path(self, to_location, distance = 0):
-        self.adjacent_paths[to_location] = distance
-
-    def print_adjacent_paths(self):
-        print(self.adjacent_paths)
-
-
-class Package:
-    def __init__(self, package_ID, address, city, state, zip_code, deliver_by, weight_in_kilograms, package_notes):
-        self.package_ID = int(package_ID)
-        self.address = address
-        self.city = city
-        self.state = state
-        self.zip_code = zip_code
-        if 'EOD' in deliver_by:
-            self.deliver_by = "EOD"
-        else: self.deliver_by = datetime.datetime.strptime(deliver_by, '%H:%M').time()
-        self.weight_in_kilograms = weight_in_kilograms
-        self.deliver_by = deliver_by
-        self.package_notes = package_notes
-
 
 class Graph:
     def __init__(self):
-        self.distances_list = []
-        self.locations_list = []
         self.adjacency_list = {}
         self.edge_weights = {}
     
--- src/Path.py
+++
@@ -1,3 +0,0 @@
-class Path:
-    def __init__(self, path_length=0):
-        self.path_length = path_length
